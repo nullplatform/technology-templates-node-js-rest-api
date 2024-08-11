1b953f4b6de80eeaecfc0ebca978f2647c8fe75e
Pablo Vilas <pablovilas89@gmail.com>
2024-08-10 23:50:56 -0300

    Add wiremock examples

--- test/functional/global.spec.js
+++ test/functional/global.spec.js
@@ -1,11 +1,30 @@
+import { Database, Wiremock } from '@nullplatform/test-container';
+import { execSync } from 'child_process';
+import Config from 'config';
 import Logger from '../../src/logger.js';
+
+const startDatabase = async () => {
+  Logger.info('Initializing test database...');
+  const { name, user, password, port } = Config.get('database');
+  await Database.start({ name, user, password, port });
+  Logger.info('Test database initialized successfully');
+  Logger.info('Running database migrations...');
+  await execSync('npm run migrate');
+  Logger.info('Database migrations completed successfully');
+};
+
+const startWiremock = async () => {
+  Logger.info('Initializing mock server...');
+  await Wiremock.start();
+  Logger.info('Mock server initialized successfully');
+};
 
 // eslint-disable-next-line mocha/no-top-level-hooks
 before(async () => {
-  Logger.info('Add your global setup here');
+  await Promise.all([startDatabase(), startWiremock()]);
 });
 
 // eslint-disable-next-line mocha/no-top-level-hooks
 after(async () => {
-  Logger.info('Add your global teardown here');
+  await Promise.all([Database.stop(), Wiremock.stop()]);
 });
